0865827406f3d36b4f2079b67e87680d44aa411d
LordPegasus <52626678+LordPegasus@users.noreply.github.com>
2020-08-16 12:12:51 +0530

    Add files via upload

---
+++ tiss_UI/footer.js
@@ -0,0 +1,11 @@
+document.write(`
+   
+  <footer id="main-footer" class="py-4 bg-primary text-white text-center">
+    Have fun learning!!! Powered by TISS
+  </footer>
+
+  <script src="assets/js/jquery-3.3.1.min.js "></script>
+  <script src="assets/js/bootstrap.bundle.min.js "></script>
+  <script src="assets/js/main.js "></script>
+
+`);
