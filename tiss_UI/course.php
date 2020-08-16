dbfbc654ade563e721123bb9b64b88e2a2cffc16
anugurishika <64183744+anugurishika@users.noreply.github.com>
2020-08-16 19:49:17 +0530

    Add files via upload
    adding course file

---
+++ tiss_UI/course.php
@@ -0,0 +1,25 @@
+<?php
+$con = mysqli_connect('localhost', 'root','','tiss');
+if (isset($_POST['addcourse'])) {
+		// receive all input values from the form
+		$coursename = $_POST['coursename'];
+		$category = $_POST['category'];
+		$description = $_POST['description'];
+		
+ $sql =  mysqli_query($con,"INSERT INTO course(`coursename`, `category`, `description`) 
+	 VALUES ('".$coursename."', '".$category."', '".description."');");
+
+ if (mysqli_query($con,$sql) === TRUE)
+	{		
+    echo "<script>alert('course created successfully');
+    </script>";
+    }
+ else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+			header('location: index.html');
+
+			}
+			
+?>
